f7fd9d2ff786b0e3d25a56d274eac18fa2c0e76e
ZaynabRameeez <zainabrameezoffl@gmail.com>
2025-10-17 01:22:47 +0530

    fix: Update author configuration

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-6 px-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {currentYear} Zaynab. All Rights Reserved.</p>
+        <p>&copy; {currentYear} ZAYNAB. All Rights Reserved.</p>
       </div>
     </footer>
   );
